c5cfd5182163d7826a8ce3603856697e7c5d9031
해퓨쳐 <kck0920@gmail.com>
2022-09-23 16:19:38 +0900

    fix_1

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-from st_aggrid import AgGrid
-from st_aggrid.grid_options_builder import GridOptionsBuilder
+# from st_aggrid import AgGrid
+# from st_aggrid.grid_options_builder import GridOptionsBuilder
 
 # 데이터 읽기
 @st.cache
