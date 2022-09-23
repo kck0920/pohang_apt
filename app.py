ffbff99325295940d2f84a40e6c0d1992c2e2c1f
해퓨쳐 <kck0920@gmail.com>
2022-09-23 16:26:38 +0900

    fix_2

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 import streamlit as st
 import pandas as pd
-# from st_aggrid import AgGrid
-# from st_aggrid.grid_options_builder import GridOptionsBuilder
+import plotly_express as px
+
+
+# emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title = "포항 아파트 실거래가 현황",
+                   page_icon = ":chart_with_upwards_trend:",
+                   layout = "wide",
+)
 
 # 데이터 읽기
 @st.cache
@@ -11,12 +17,70 @@ def data_upload():
 
 df = data_upload()
 st.dataframe(df)
-# gd = GridOptionsBuilder.from_dataframe(df)
-# gd.configure_pagination(enabled=True)
-# gd.configure_default_column(editable=True, groupable=True)
 
-# sel_mode = st.radio('Selection Type', options=['single', 'multiple'])
-# gd.configure_selection(selection_mode=sel_mode, use_checkbox=True)
-# grid_options = gd.build()
-# st.header("포항 주요아파트 실거래가 현황")
-# AgGrid(df, gridOptions=grid_options) 
+# ----- 단지 선택 -----
+단지명 = st.sidebar.selectbox(
+    "단지명 선택",
+    options = df["단지명"].unique()
+)
+
+df_selection = df.query(
+    "단지명 == @단지명"
+)
+
+# ----- MAINPAGE -----
+st.title(":chart_with_upwards_trend: 2022년 포항 주요 아파트 실거래 현황")
+st.markdown("#")
+
+
+# ----- 최고가 저저가 보여주기 -----
+low_price = df_selection["거래금액(만원)"].min()
+high_price = df_selection["거래금액(만원)"].max()
+danji = df["단지명"].unique()
+
+left_column, middle_column, right_column = st.columns(3)
+with left_column:
+    st.subheader("단지명:")
+    st.subheader(f"{danji}")
+with middle_column:
+    st.subheader("최저금액:")
+    st.subheader(f"{low_price}만원")
+with right_column:
+    st.subheader("최고금액:")
+    st.subheader(f"{high_price}만원")
+
+st.markdown("---")
+
+# ----- 차트 그리기 -----
+contract_date = df_selection["계약일"]
+contract_price = df_selection["거래금액(만원)"]
+
+
+fig = px.line(
+    df_selection,
+    x = contract_date,
+    y = contract_price,
+    title = '< 거래일자 별 실거래금액 추이 >',  # 그래프 타이틀 지정
+    text = contract_price,
+    markers = True,
+)
+
+fig.update_layout(
+    width = 900,
+    height = 550,
+    paper_bgcolor = "#0083B8",
+    plot_bgcolor = "rgb(223, 100, 200)",
+    xaxis=dict(showgrid=False),
+    yaxis=dict(showgrid=False),
+    title_font_size = 28,
+    # font_color = "black",
+    font_size = 14,
+)
+
+fig.update_traces(
+    line=dict(width=3)
+)
+
+fig.update_xaxes(title_font_family="Arial")
+
+st.plotly_chart(fig)
